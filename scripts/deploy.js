6e07e362278589923e85301a0f6ca5edb786e2bd
Gustavo Diel <gustavo.diel@trustedhealth.com>
2022-03-16 19:41:18 -0300

    add docker-compose and more queue commands

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,82 @@
-deploy = require('./heroku')
+const deploy = require('./heroku')
+const { createClient } = require('redis');
+
+let client;
+
+(async () => {
+  client = createClient();
+
+  client.on('error', (err) => console.log('Redis Client Error', err));
+
+  await client.connect();
+})();
+
+const run = (msg, branch, env) => {
+  msg.send(`I am going to ship ${branch} to ${env}`);
+
+  deploy(msg, branch, async (body) => {
+    if (body.includes('Push rejected') || body.includes('ERR! build error')) {
+      robot.emit = { "color": "#ff0000", "fallback": "fallback", text: "Err Text", title: "Err Title", title_link: "err_link" }
+
+      msg.reply('Build failed! Try again looser')
+    } else {
+      robot.emit = { "color": "#7CD197", "fallback": "fallback", text: "Text", title: "Title", title_link: "" }
+
+      msg.reply('Successfully builded')
+    }
+    const removed = await client.lPop(`queue_${env}`)
+    msg.send(`${removed} is done! Going to the next one!`)
+    const queue_data = await client.lIndex(`queue_${env}`, 0)
+    run(msg, queue_data.branch, queue_data.env)
+  })
+}
 
 module.exports = (robot) => {
-  robot.respond(/ship(!)? ([^\s]+) to ([^\s\/]+)$/i, (msg) => {
+  robot.respond(/ship(!)? ([^\s]+) to ([^\s\/]+)$/i, async (msg) => {
     const priority = msg.match[1] === '!';
     const branch = msg.match[2];
     const env = msg.match[3];
-    msg.send(`I am going to ship ${branch} to ${env} - ${priority ? 'With high priority' : 'With regular priority'}`);
+    const queue_name = `queue_${env}`;
 
-    deploy(msg, branch);
+    let len = await client.lLen(queue_name)
+    let elements = await client.lRange(queue_name, 0, len)
+
+    const repeated = elements.find(unparsed => { return unparsed === JSON.stringify({ branch, env }) })
+    if (!!repeated) {
+      return msg.send(`Slow down dude, your branch ${branch} is already in the queue!`)
+    }
+
+    await client.rPush(queue_name, JSON.stringify({ branch, env }))
+    const current_queue = JSON.parse(await client.lIndex(queue_name, 0))
+
+    if (current_queue.branch === branch && current_queue.env === env) {
+      run(msg, branch, env)
+    } else {
+      msg.send(`Hey dude, you were added to the queue for ${env} - current: ${current_queue.branch}, yours: ${branch}`)
+    }
   });
+
+  robot.respond(/queue empty ([^\s\/]+)$/i, async (msg) => {
+    const env = msg.match[1] || 'prod';
+
+    const queue_name = `queue_${env}`;
+
+    await client.del(queue_name)
+
+    msg.send(`Queue ${queue_name} cleared`)
+  })
+
+  robot.respond(/queue ([^\s\/]+)$/i, async (msg) => {
+    const env = msg.match[1] || 'prod';
+
+    const queue_name = `queue_${env}`;
+
+    let len = await client.lLen(queue_name)
+    let elements = await client.lRange(queue_name, 0, len)
+
+    const message = elements.map(element => JSON.parse(element).branch).join(', ')
+
+    msg.send(`Current queue ${queue_name}: ${message}`)
+  })
+
 }
--- scripts/heroku.js
+++ scripts/heroku.js
@@ -5,8 +5,10 @@
 const Heroku = require('heroku-client')
 const { Octokit } = require('@octokit/rest')
 const { createTokenAuth } = require('@octokit/auth-token')
+const https = require('https');
 
-module.exports = async (msg, branch) => {
+
+module.exports = async (msg, branch, callback) => {
   if (!process.env.HUBOT_HEROKU_KEY) {
     return msg.reply("To deploy to Heroku you need a HEROKU_KEY")
   }
@@ -25,22 +27,41 @@ module.exports = async (msg, branch) => {
   const heroku = new Heroku({ token: process.env.HUBOT_HEROKU_KEY })
 
   const apps = await heroku.get('/apps')
-  const app = apps.find(app => app.name == appname)
+  const app = apps.find(app => app.name === appname)
 
   if (!app) {
     msg.reply(`App ${appname} not found`)
+    return callback(undefined)
   }
 
   // build app
   try {
-    build = await heroku.post(`/apps/${app.name}/builds`, {
+    const build = await heroku.post(`/apps/${app.name}/builds`, {
       body: {
         source_blob: { url: url }
       }
-    })
+    });
 
-    msg.reply('Successfully builded', JSON.stringify(build.output_stream_url))
+    msg.reply(`Waiting for build ${build.id} - ${build.output_stream_url}`)
+
+    try {
+      let body = '';
+      https.get(build.output_stream_url, res => {
+        res.on('data', chunk => {
+          body += chunk;
+        });
+
+        res.on('end', () => {
+          callback(body)
+        });
+      });
+
+    } catch(err) {
+      msg.reply("http err:    ", JSON.stringify(err))
+      return callback(undefined)
+    }
   } catch(err) {
     msg.reply("err:    ", JSON.stringify(err))
+    return callback(undefined)
   }
 }
